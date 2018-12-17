985334133fb3ce530643e4cfc98345b6ad91f2bb
FranciscoThiesen <franciscogthiesen@gmail.com>
2018-12-17 11:31:16 -0200

    Testing recursiveSegmentTree implementation

--- DataStructures/Segtree/iterativeSeg.cpp
+++ DataStructures/Segtree/iterativeSeg.cpp
@@ -10,8 +10,6 @@ using namespace std;
 typedef vector<int> vi;
 typedef pair<int, int> ii;
 
-// Ja foi testado....
-// Olhar o tutorial caso combine nao seja comutativa (Efficient and easy segment trees)
 template <typename T>
 struct seg{
 	int N;
@@ -19,24 +17,19 @@ struct seg{
 	T id;
 
 	// mudar de acordo com o problema!
-	T combine(const T& a, const T& b){
-		return min(a,b);
-	}
+	T combine(const T& a, const T& b){ return min(a,b); }
 
-	seg(int n, T dummy) : N(n), id(dummy) {
-		Tree.assign(2*N, id);
-	}
-	void build(){
-		for(int idx = N-1; idx > 0; --idx) Tree[idx] = combine(Tree[(idx << 1)], Tree[(idx << 1) | 1]);
-	}
+	seg(int n, T dummy) : N(n), id(dummy) { Tree.assign(2*N, id); }
+	void build() { for(int idx = N-1; idx > 0; --idx) Tree[idx] = combine(Tree[(idx << 1)], Tree[(idx << 1) | 1]); }
 	void update(int p, T val){
 		Tree[p += N] = val;
-		while(p > 1){
+		while(p > 1) {
 			Tree[p>>1] = combine(Tree[p], Tree[p^1]);
 			p >>= 1;
 		}
 	}
-	// Query of [i, j)
+	
+    // Query of [l, r)
 	T query(int l, int r){
 		T res = id;
 		for(l += N, r += N; l < r; l >>= 1, r >>= 1){
@@ -47,6 +40,6 @@ struct seg{
 	}
 };
 
-int main(){
+int main() {
 	return 0;
 }
